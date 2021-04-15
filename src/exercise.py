3c5eb28e212cee397253047dc2dd5f8301ed9c29
pmatyus <>
2021-04-15 10:53:05 +0000

    Added variable and divided by 2

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    first = int(input('Give the first number:'))
+    second = int(input('Give the second number:'))
+    print('The average is ' + str((first + second)/2))
 
 if __name__ == '__main__':
     main()
